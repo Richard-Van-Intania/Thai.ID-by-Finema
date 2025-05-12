7af5f5c5fb54321c6b1edb5dd421ea82c7d986f2
Ittipat Pattum <nampu@finema.co>
2025-05-12 13:21:23 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/Navigations.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/Navigations.kt
@@ -1,9 +1,9 @@
 package co.finema.thaidotidbyfinema.uis
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material.icons.rounded.History
+import androidx.compose.material.icons.rounded.Description
 import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import co.finema.thaidotidbyfinema.R
 
@@ -25,9 +25,9 @@ sealed class Screen(val route: String, val name: Int? = null, val icon: ImageVec
 
   object HomeTabNav : Screen("HomeTabNav", R.string.home, Icons.Rounded.Home)
 
-  object HistoryTabNav : Screen("HistoryTabNav", R.string.history, Icons.Rounded.History)
+  object HistoryTabNav : Screen("HistoryTabNav", R.string.history, Icons.Rounded.Description)
 
-  object ProfileTabNav : Screen("ProfileTabNav", R.string.profile, Icons.Rounded.AccountCircle)
+  object ProfileTabNav : Screen("ProfileTabNav", R.string.profile, Icons.Rounded.Person)
 
   object SelectLayoutScreenNav : Screen("SelectLayoutScreenNav")
 
