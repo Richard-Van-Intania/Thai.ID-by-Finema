a5d0d0f91ace0a9dac9a4f9730d01a346a1e9da5
Ittipat Pattum <nampu@finema.co>
2025-05-15 00:04:25 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/profile/SupportScreen.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/profile/SupportScreen.kt
@@ -70,8 +70,9 @@ fun SupportScreen(navController: NavController) {
                             .appendQueryParameter("body", body)
                             .build()
                     val intent = Intent(Intent.ACTION_SENDTO).apply { data = uri }
-                    if (intent.resolveActivity(context.packageManager) != null)
-                        context.startActivity(intent)
+                    if (intent.resolveActivity(context.packageManager) != null) {
+                      context.startActivity(intent)
+                    }
                   }) {
                     Text(
                         text = email,
