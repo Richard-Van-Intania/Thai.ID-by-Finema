9aa25da04c9b683db9f41d4c05f576ec6038b11a
Ittipat Pattum <nampu@finema.co>
2025-05-14 19:52:48 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/passcodes/ConfirmPasscodeChangeFullscreen.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/passcodes/ConfirmPasscodeChangeFullscreen.kt
@@ -3,4 +3,5 @@ package co.finema.thaidotidbyfinema.uis.screens.passcodes
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
-@Composable fun ConfirmPasscodeChangeFullscreen(navController: NavController, tapPasscode: String) {}
+@Composable
+fun ConfirmPasscodeChangeFullscreen(navController: NavController, tapPasscode: String) {}
