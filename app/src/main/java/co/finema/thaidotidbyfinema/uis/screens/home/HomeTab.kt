237abe56ec92f1c5e9e16338324d36dcc520f494
Ittipat Pattum <nampu@finema.co>
2025-05-23 14:38:33 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/HomeTab.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/HomeTab.kt
@@ -2,27 +2,16 @@
 
 package co.finema.thaidotidbyfinema.uis.screens.home
 
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import co.finema.thaidotidbyfinema.uis.Screen
-import co.finema.thaidotidbyfinema.uis.whiteBG
+import co.finema.thaidotidbyfinema.uis.greenDev
 
 @Composable
 fun HomeTab(navController: NavController) {
-  Scaffold(
-      backgroundColor = whiteBG,
-      floatingActionButton = {
-        FloatingActionButton(onClick = {
-            navController.navigate(route = Screen.SelectLayoutScreen.route)
-        }) {
-          Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
-        }
-      }) {
-        it
-      }
+  Scaffold(backgroundColor = greenDev) { Column(modifier = Modifier.padding(it)) { Text("ssssssssssss") } }
 }
