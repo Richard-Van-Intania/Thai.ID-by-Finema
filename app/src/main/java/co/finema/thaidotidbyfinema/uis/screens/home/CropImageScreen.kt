fa86b82b0d517d83b5bf46160609df0d890604f2
Ittipat Pattum <nampu@finema.co>
2025-05-31 18:14:34 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/CropImageScreen.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/CropImageScreen.kt
@@ -7,22 +7,20 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import co.finema.thaidotidbyfinema.uis.black
 import com.tanishranjan.cropkit.ImageCropper
 import com.tanishranjan.cropkit.rememberCropController
 
@@ -41,16 +39,13 @@ fun CropImageScreen(navController: NavController, imageUri: MutableState<Uri?>) 
   BackHandler(enabled = true) {}
   val context = LocalContext.current
   var selectedBitmap by remember { mutableStateOf<Bitmap?>(null) }
-  imageUri.value?.let { selectedBitmap = getBitmapFromUri(context, it) }
-  Scaffold() {
-    if (selectedBitmap == null)
-      Box(modifier = Modifier.fillMaxSize().padding(it), contentAlignment = Alignment.Center) {
-        CircularProgressIndicator()
-      }
-    else
-      Column(modifier = Modifier.fillMaxSize().padding(it)) {
-        val cropController = rememberCropController(bitmap = selectedBitmap!!)
-        ImageCropper(cropController = cropController)
-      }
+  LaunchedEffect(imageUri.value) {
+    imageUri.value?.let { selectedBitmap = getBitmapFromUri(context, it) }
+  }
+  selectedBitmap?.let { bitmap ->
+    val cropController = rememberCropController(bitmap = bitmap)
+    Scaffold(backgroundColor = black) {
+      ImageCropper(modifier = Modifier.fillMaxSize().padding(it), cropController = cropController)
+    }
   }
 }
