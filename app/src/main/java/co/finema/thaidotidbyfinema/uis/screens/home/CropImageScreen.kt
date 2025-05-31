8b8e4a5eba90dad4efe415c4547d3063494a2cff
Ittipat Pattum <nampu@finema.co>
2025-05-31 20:57:28 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/CropImageScreen.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/CropImageScreen.kt
@@ -2,9 +2,6 @@
 
 package co.finema.thaidotidbyfinema.uis.screens.home
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,14 +16,8 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -34,30 +25,17 @@ import androidx.navigation.NavController
 import co.finema.thaidotidbyfinema.R
 import co.finema.thaidotidbyfinema.uis.black
 import co.finema.thaidotidbyfinema.uis.white
-import com.tanishranjan.cropkit.ImageCropper
-import com.tanishranjan.cropkit.rememberCropController
-
-fun getBitmapFromUri(context: Context, uri: Uri): Bitmap? {
-  return try {
-    val inputStream = context.contentResolver.openInputStream(uri)
-    BitmapFactory.decodeStream(inputStream).also { inputStream?.close() }
-  } catch (e: Exception) {
-    e.printStackTrace()
-    null
-  }
-}
+import io.moyuru.cropify.Cropify
+import io.moyuru.cropify.CropifyOption
+import io.moyuru.cropify.CropifySize.PercentageSize.Companion.FullSize
+import io.moyuru.cropify.rememberCropifyState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CropImageScreen(navController: NavController, imageUri: MutableState<Uri?>) {
   BackHandler(enabled = true) {}
-  val context = LocalContext.current
-  var selectedBitmap by remember { mutableStateOf<Bitmap?>(null) }
-  LaunchedEffect(imageUri.value) {
-    imageUri.value?.let { selectedBitmap = getBitmapFromUri(context, it) }
-  }
-  selectedBitmap?.let { bitmap ->
-    val cropController = rememberCropController(bitmap = bitmap)
+  val state = rememberCropifyState()
+  imageUri.value?.let { imgUri ->
     Scaffold(
       topBar = {
         CenterAlignedTopAppBar(
@@ -70,7 +48,7 @@ fun CropImageScreen(navController: NavController, imageUri: MutableState<Uri?>) 
             )
           },
           actions = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { state.crop() }) {
               Icon(imageVector = Icons.Rounded.Check, contentDescription = null, tint = white)
             }
           },
@@ -84,7 +62,17 @@ fun CropImageScreen(navController: NavController, imageUri: MutableState<Uri?>) 
       },
       backgroundColor = black,
     ) {
-      ImageCropper(modifier = Modifier.fillMaxSize().padding(it), cropController = cropController)
+      Cropify(
+        modifier = Modifier.fillMaxSize().padding(it),
+        uri = imgUri,
+        state = state,
+        onImageCropped = {
+          //
+          println(it.width)
+        },
+        onFailedToLoadImage = {},
+        option = CropifyOption(frameSize = FullSize),
+      )
     }
   }
 }
