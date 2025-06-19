28653fba0856a2fadb3eb7f4ba81f2e4e52311c3
Ittipat Pattum <nampu@finema.co>
2025-06-19 15:17:02 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryDao.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryDao.kt
@@ -1,6 +1,23 @@
 package co.finema.thaidotidbyfinema.databases.layouthistories
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
-interface LayoutHistoryDao {}
+interface LayoutHistoryDao {
+    @Query("SELECT * from layout_history ORDER BY id ASC")
+    suspend fun getAllLayoutHistory(): List<LayoutHistory>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertLayoutHistory(layoutHistory: LayoutHistory)
+
+    @Query("UPDATE layout_history SET date_last_used = :dateLastUsed WHERE id = :id")
+    suspend fun updateLastUsed(id: Int, dateLastUsed: String)
+
+    @Query("UPDATE layout_history SET user_defined_name = :userDefinedName WHERE id = :id")
+    suspend fun updateUserDefinedName(id: Int, userDefinedName: String)
+
+    @Query("DELETE from layout_history WHERE id = :id") suspend fun deleteLayoutHistory(id: Int)
+}
--- app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryDatabase.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryDatabase.kt
@@ -1,3 +1,28 @@
 package co.finema.thaidotidbyfinema.databases.layouthistories
 
-class LayoutHistoryDatabase {}
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [LayoutHistory::class], version = 1, exportSchema = false)
+abstract class LayoutHistoryDatabase : RoomDatabase() {
+    abstract fun layoutHistoryDao(): LayoutHistoryDao
+
+    companion object {
+        @Volatile private var Instance: LayoutHistoryDatabase? = null
+
+        fun getDatabase(context: Context): LayoutHistoryDatabase {
+            return Instance
+                ?: synchronized(this) {
+                    Room.databaseBuilder(
+                            context,
+                            LayoutHistoryDatabase::class.java,
+                            "layout_history_database",
+                        )
+                        .build()
+                        .also { Instance = it }
+                }
+        }
+    }
+}
