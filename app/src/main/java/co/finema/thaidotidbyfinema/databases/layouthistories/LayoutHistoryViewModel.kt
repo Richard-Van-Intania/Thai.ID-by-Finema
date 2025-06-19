c1fb5fb200ec91a876b3fd0b4c4a44dc47d1c69b
Ittipat Pattum <nampu@finema.co>
2025-06-19 15:25:38 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryViewModel.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/databases/layouthistories/LayoutHistoryViewModel.kt
@@ -1,3 +1,21 @@
 package co.finema.thaidotidbyfinema.databases.layouthistories
 
-class LayoutHistoryViewModel {}
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class LayoutHistoryViewModel(private val layoutHistoryDao: LayoutHistoryDao) : ViewModel() {
+    private val _layoutHistory = MutableStateFlow<List<LayoutHistory>>(emptyList())
+    val layoutHistory: StateFlow<List<LayoutHistory>>
+        get() = _layoutHistory
+
+    init {
+        loadLayoutHistory()
+    }
+
+    fun loadLayoutHistory() {
+        viewModelScope.launch { _layoutHistory.value = layoutHistoryDao.getAllLayoutHistory() }
+    }
+}
