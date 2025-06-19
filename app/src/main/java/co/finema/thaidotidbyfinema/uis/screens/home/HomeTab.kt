b65044679e67a1663f6f68051b71640383191579
Ittipat Pattum <nampu@finema.co>
2025-06-19 18:31:42 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/HomeTab.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/HomeTab.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -286,8 +289,18 @@ fun HomeTab(navController: NavController, layoutHistoryViewModel: LayoutHistoryV
                             )
                         }
                 }
-                // here below
-
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top,
+                ) {
+                    items(layoutHistory) {
+                        Text(
+                            "id:${it.id} dateCreated:${it.dateCreated} dateLastUsed:${it.dateLastUsed}",
+                            fontSize = 16.sp,
+                        )
+                    }
+                }
             }
         }
     }
