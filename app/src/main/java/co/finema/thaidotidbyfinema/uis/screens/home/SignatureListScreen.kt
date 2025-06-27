5981cd9555fee77aa9a6f8a577e51c2aea1d556d
Ittipat Pattum <nampu@finema.co>
2025-06-28 00:11:13 +0700

    save

--- app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/SignatureListScreen.kt
+++ app/src/main/java/co/finema/thaidotidbyfinema/uis/screens/home/SignatureListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -30,19 +31,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import co.finema.thaidotidbyfinema.R
 import co.finema.thaidotidbyfinema.databases.signatureimages.SignatureImageViewModel
 import co.finema.thaidotidbyfinema.uis.blue02
+import co.finema.thaidotidbyfinema.uis.gradient
+import co.finema.thaidotidbyfinema.uis.lightBlue07
 import co.finema.thaidotidbyfinema.uis.neutral01
 import co.finema.thaidotidbyfinema.uis.primaryBlack
 import co.finema.thaidotidbyfinema.uis.white
@@ -52,6 +61,63 @@ import coil.compose.rememberAsyncImagePainter
 @Composable
 fun SignatureListScreen(navController: NavController, signatureImageViewModel: SignatureImageViewModel, currentSignatureImageId: MutableIntState) {
     val signatureImage by signatureImageViewModel.signatureImage.collectAsState()
+    var deleteSignatureImageId by remember { mutableIntStateOf(0) }
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    if (showDeleteDialog) {
+        Dialog(onDismissRequest = {}) {
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(white)
+                    .fillMaxWidth()
+               ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp), horizontalAlignment = Alignment.CenterHorizontally
+                      ) {
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Text(
+                        text = stringResource(R.string.do_you_want_to_delete_this_document), color = primaryBlack, fontSize = 24.sp, fontWeight = FontWeight.W700, textAlign = TextAlign.Center
+                        )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Box(
+                            modifier = Modifier
+                                .height(56.dp)
+                                .weight(1f)
+                                .clip(RoundedCornerShape(56.dp))
+                                .background(brush = gradient)
+                                .clickable(
+                                    onClick = {
+                                        showDeleteDialog = false
+                                        signatureImageViewModel.removeSignatureImage(deleteSignatureImageId)
+                                    }),
+                            contentAlignment = Alignment.Center,
+                           ) {
+                            Text(text = stringResource(R.string.delete), color = white, fontSize = 24.sp, fontWeight = FontWeight.W700)
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Box(
+                            modifier = Modifier
+                                .height(56.dp)
+                                .weight(1f)
+                                .background(white)
+                                .border(width = 2.dp, color = lightBlue07, shape = RoundedCornerShape(56.dp))
+                                .clip(RoundedCornerShape(56.dp))
+                                .clickable(onClick = {
+                                    showDeleteDialog = false
+                                }),
+                            contentAlignment = Alignment.Center,
+                           ) {
+                            Text(text = stringResource(R.string.cancel), color = lightBlue07, fontSize = 24.sp, fontWeight = FontWeight.W700)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                }
+            }
+        }
+    }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -96,7 +162,8 @@ fun SignatureListScreen(navController: NavController, signatureImageViewModel: S
                             .background(neutral01)
                             .clickable(
                                 onClick = {
-                                    signatureImageViewModel.removeSignatureImage(signature.id)
+                                    currentSignatureImageId.intValue = signature.id
+                                    navController.popBackStack()
                                 }), contentAlignment = Alignment.Center
                            ) {
                         Row(
@@ -108,15 +175,20 @@ fun SignatureListScreen(navController: NavController, signatureImageViewModel: S
                                 painter = rememberAsyncImagePainter(model = signature.fileName.toUri()), contentDescription = null,
                                  )
                             Spacer(modifier = Modifier.weight(1f))
-                            Text(text = "Last item")
+                            Image(
+                                painter = painterResource(id = R.drawable.vector), contentDescription = null, modifier = Modifier
+                                    .height(24.dp)
+                                    .clip(RoundedCornerShape(24.dp))
+                                    .clickable {
+                                        deleteSignatureImageId = signature.id
+                                        showDeleteDialog = true
+                                    })
                         }
                         }
                         Spacer(modifier = Modifier.height(16.dp))
                     }
-
                 }
             }
         }
-
     }
 }
